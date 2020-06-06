44fac1304a1a39e293ed1b4ed890fdbdffc640ce
Degeri <41839293+degeri@users.noreply.github.com>
2020-06-06 23:27:57 +0200

    Initial commit to add the node stats script

---
+++ nodes/get_node_average.py
@@ -0,0 +1,101 @@
+import requests
+import json
+import time
+import datetime
+import operator
+
+
+# Send request to API and return json data as json object
+def get_data():
+    url = apipoint+'q=SELECT count(distinct("addr")) FROM "peers" WHERE time >= '+start_date_unix+ 'ms and time <= '+end_date_unix+'ms GROUP BY time(1d), "useragent_tag" fill(none)'
+    print("===========================================================")
+    print("Getting data from " + url)
+    print("===========================================================")
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        return response_json
+    else:
+        print("Check Connection")
+        exit
+
+def print_node_data(interested_useragents_percentage):
+    print("===========================================================")
+
+    print_list = "Average version distribution for " + start_date.strftime('%B') + ": "
+
+    # Process and print dcrd useragents.
+    for useragent in interested_useragents_percentage:
+        if "dcrd" in str(useragent[0][0]):
+            templist = useragent[0][0].split("/")
+            print_list= print_list + str(round(useragent[0][2],2)) + "%  " + templist[2] + ", "
+
+    # Process and print dcrwallet useragents. 
+    for useragent in interested_useragents_percentage:
+        if "dcrwallet" in str(useragent[0][0]):
+            templist = useragent[0][0].split("/")
+            print_list= print_list +  str(round(useragent[0][2],2)) + "%  " + templist[2] + ", "
+    
+    print(print_list)
+    print("===========================================================")
+
+
+#API endpoint
+apipoint = "https://charts.dcr.farm/api/datasources/proxy/1/query?db=decred&"
+
+#Change these dates for your range
+start_date = datetime.datetime(2020,5,1,0,0,0,0)
+end_date = datetime.datetime(2020,5,31,23,59,59,0)
+
+#Convert datetime to unix time format as required by the api
+start_date_unix = str(start_date.replace(tzinfo=datetime.timezone.utc).timestamp())[:-2]+"000"
+end_date_unix = str(end_date.replace(tzinfo=datetime.timezone.utc).timestamp())[:-2]+"000"
+
+# The versions we are intrested in
+interested_versions_list = ["1.4.0","1.5.0","1.5.1","1.6.0"]
+
+#Get the data as JSON from the API endpoint
+get_data_json = get_data()
+
+#Uncomment to get raw json
+#print(get_data_json)
+
+useragent_avg_list = []
+
+#Process json into list of format [['useragent','averagenodes'],['useragent2','averagenodes2']...]
+for series in get_data_json['results'][0]['series']:
+    total = 0
+    count = 0
+    data_useragent = series['tags']['useragent_tag']
+    for data_point in series['values']:
+        total = total + data_point[1]
+        count = count+1
+    average=total/count
+    useragent_avg_list.append([data_useragent,average])
+
+
+interested_useragents = []
+totalcount_interested = 0
+
+# Filter out only useragents that contain strings form `interested_versions_list` also calulate the sum of all interested nodes into totalcount_interested.
+for useragent in useragent_avg_list:
+    for version in interested_versions_list:
+        if str(version) in str(useragent[0]):
+            interested_useragents.append(useragent)
+            totalcount_interested = totalcount_interested + useragent[1]
+
+
+#Sort decending
+interested_useragent_ordered = sorted(interested_useragents, key=operator.itemgetter(1), reverse=True)
+
+
+interested_useragents_percentage = []
+
+#Calculate and add another column into list. [['useragent','averagenodes','average%'],['useragent2','averagenodes2',average2%]...]
+for intrest in interested_useragent_ordered:
+    percentage = intrest[1]/(totalcount_interested/100)
+    intrest.append(percentage)
+    interested_useragents_percentage.append([intrest])
+
+#Send data into print_node_data function to output in desired format.
+print_node_data(interested_useragents_percentage)
