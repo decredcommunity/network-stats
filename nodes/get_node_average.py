b9ead0c5c52e5f53511000496bd9c192fc8a69aa
xaur <24484727+xaur@users.noreply.github.com>
2020-07-23 17:54:38 +0000

    nodes: combine api url and add str formatting

--- nodes/get_node_average.py
+++ nodes/get_node_average.py
@@ -23,10 +23,7 @@ def get_dcrfarm_data(start_date, end_date):
     start_date_unix = datetime_to_unix_millis(start_date)
     end_date_unix = datetime_to_unix_millis(end_date)
 
-    #API endpoint
-    apipoint = "https://charts.dcr.farm/api/datasources/proxy/1/query?db=decred&"
-
-    url = apipoint+'q=SELECT count(distinct("addr")) FROM "peers" WHERE time >= '+start_date_unix+ 'ms and time <= '+end_date_unix+'ms GROUP BY time(1d), "useragent_tag" fill(none)'
+    url = 'https://charts.dcr.farm/api/datasources/proxy/1/query?db=decred&q=SELECT count(distinct("addr")) FROM "peers" WHERE time >= {}ms and time <= {}ms GROUP BY time(1d), "useragent_tag" fill(none)'.format(start_date_unix, end_date_unix)
 
     print("===========================================================")
     print("Getting data from " + url)
